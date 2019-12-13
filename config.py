e0290a2399b682fbcfc7d81db84cb0460ce47162
gzapodea <gzapodea@cisco.com>
2019-12-12 17:03:31 -0800

    initial commit

--- config.py
+++ config.py
@@ -2,8 +2,6 @@
 # -*- coding: utf-8 -*-
 """
 
-Cisco DNA Center Client Information using the MAC Address
-
 Copyright (c) 2019 Cisco and/or its affiliates.
 
 This software is licensed to you under the terms of the Cisco Sample
